b1c19803ca604cb7a16e70360e13cadd61dd0697
Batman99SD <127743755+Batman99SD@users.noreply.github.com>
2023-09-19 17:08:45 +0300

    Update print_num.c

--- print_num.c
+++ print_num.c
@@ -1,21 +1,36 @@
 #include "main.h"
 
-/**
- * print_n - prints numbers
- * @list: list of arguments
- * Return: characters count
- */
+int print_n(va_list list) {
+        int n = va_arg(list, int);
+        int count = 0;
+        int i;
+        int temp;
+        int num_digits;
 
-int print_n(va_list list)
-{
-	int n = va_arg(list, int);
-	int count = 0;
-	char buffer[20];
-	int len = snprintf(buffer, sizeof(buffer), "%d", n);
-	if (len > 0)
-	{
-		write(1, buffer, len);
-		count += len;
-	}
-	return count;
+        /* Handle negative numbers */
+        if (n < 0) {
+                write(1, "-", 1);
+                n = -n;
+                count++;
+        }
+
+
+        /* Calculate the number of digits in n */
+        num_digits = 1;
+        while (temp >= 10) {
+                temp /= 10;
+                num_digits++;
+        }
+
+
+        /* Extract and print each digit */
+        for (i = 0; i < num_digits; i++) {
+                int digit = n % 10;
+                char digit_char = '0' + digit;
+                write(1, &digit_char, 1);
+                n /= 10;
+                count++;
+        }
+
+        return count;
 }
