c4b8844bd5af75d9c9f1b6aeda00b257e7fa1fd7
Wafaa <wafaagaafar@gmail.com>
2023-09-19 13:50:05 +0200

    checking

--- _print.c
+++ _print.c
@@ -1,3 +1,4 @@
+#include <limits.h>
 #include "main.h"
 
 int (*pick_f(const char *format))(va_list)
@@ -7,8 +8,8 @@ int (*pick_f(const char *format))(va_list)
 		{"c", print_c},
 		{"s", print_s},
 		{"d", print_n},
-		{NULL, NULL}
-	};
+		{"i", print_n},
+		{NULL, NULL}};
 
 	for (i = 0; pick[i].f_spec; i++)
 	{
@@ -76,12 +77,34 @@ int _printf(const char *format, ...)
 
 int main(void)
 {
-	char c = 'A';
-	char b = 'b';
-	int d = 1327;
-	char *str = "This is a string";
-	_printf("This is a couple of characters %c %c\n", c, b);
-	_printf("%s\n", str);
-	_printf("%d\n", d);
-	return 0;
+	int len;
+	int len2;
+	unsigned int ui;
+	void *addr;
+
+	len = _printf("Let's try to printf a simple sentence.\n");
+	len2 = printf("Let's try to printf a simple sentence.\n");
+	ui = (unsigned int)INT_MAX + 1024;
+	addr = (void *)0x7ffe637541f0;
+	_printf("Length:[%d, %i]\n", len, len);
+	printf("Length:[%d, %i]\n", len2, len2);
+	_printf("Negative:[%d]\n", -762534);
+	printf("Negative:[%d]\n", -762534);
+	_printf("Unsigned:[%u]\n", ui);
+	printf("Unsigned:[%u]\n", ui);
+	_printf("Unsigned octal:[%o]\n", ui);
+	printf("Unsigned octal:[%o]\n", ui);
+	_printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
+	printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
+	_printf("Character:[%c]\n", 'H');
+	printf("Character:[%c]\n", 'H');
+	_printf("String:[%s]\n", "I am a string !");
+	printf("String:[%s]\n", "I am a string !");
+	_printf("Address:[%p]\n", addr);
+	printf("Address:[%p]\n", addr);
+	len = _printf("Percent:[%%]\n");
+	len2 = printf("Percent:[%%]\n");
+	_printf("Len:[%d]\n", len);
+	printf("Len:[%d]\n", len2);
+	return (0);
 }
