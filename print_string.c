c4c7ea6957d9a9863b969456bd76904e5b8e959b
Wafaa <107814989+wafaj3fr@users.noreply.github.com>
2023-09-19 15:53:00 +0200

    Update print_string.c

--- print_string.c
+++ print_string.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * print_s - prints strings
+ * @list: list of arguments
+ * Return: characters count
+*/
+
 int print_s(va_list list)
 {
     const char *str = va_arg(list, const char *);
